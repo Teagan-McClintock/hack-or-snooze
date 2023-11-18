27ad5a49d3134027b79b0a06eb7e9ba31f6acd99
Max Shpungin <maxsdance@gmail.com>
2023-11-17 16:12:59 -0800

    buttons can toggle! favs are broken :(

--- js/stories.js
+++ js/stories.js
@@ -25,7 +25,9 @@ function generateStoryMarkup(story) {
 
   const hostName = story.getHostName();
   const favClassText = isFav(story) ? 'class="favorite"' : "";
-  return $(`
+  const $favStar = $('<i class="bi bi-star-fill"></i>');
+  const $markedUpStory =
+    $(`
       <li id="${story.storyId}" ${favClassText}>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
@@ -35,27 +37,41 @@ function generateStoryMarkup(story) {
         <small class="story-user">posted by ${story.username}</small>
       </li>
     `);
+
+  if (favClassText) {
+    $markedUpStory.append($favStar);
+  };
+
+  return $markedUpStory;
 }
 
 
 /**Takes in a story and identifies whether it is a favorite of the current
  * user */
 
-function isFav(story){
-  for (let favStory of currentUser.favorites){
-    if (favStory.storyId === story.storyId){
+function isFav(story) {
+  for (let favStory of currentUser.favorites) {
+    if (favStory.storyId === story.storyId) {
       return true;
     }
   }
   return false;
 }
 
-/**Generates and returns html text for favorite and remove favorite buttons */
+/**Generates and returns html text for favorite and remove favorite buttons.
+ * Depending on the value of isFav, either the favorite or remove favorite button
+ * will be marked as a hidden class
+*/
 
-function generateFavoriteButtons() {
+function generateFavoriteButtons(isFav) {
+  if (isFav) {
+    return $(`
+   <button class = "favorite-button hidden">Favorite</button>
+   <button class = "remove-favorite-button">Remove Favorite</button>`);
+  }
   return $(`
   <button class = "favorite-button">Favorite</button>
-  <button class = "remove-favorite-button">Remove Favorite</button>`);
+  <button class = "remove-favorite-button hidden">Remove Favorite</button>`);
 }
 
 /** Gets list of stories from server, generates their HTML, and puts on page. */
@@ -68,20 +84,29 @@ function putStoriesOnPage() {
   // loop through all of our stories and generate HTML for them
   for (let story of storyList.stories) {
     const $story = generateStoryMarkup(story);
-    const $favButtons = generateFavoriteButtons();
+    const isFavorite = isFav(story);
+    const $favButtons = generateFavoriteButtons(isFavorite);
     $story.append($favButtons);
+    // if the story is a favorite
+    //  hide favorite button
+    //  else hide remove fav button
+    // toggleButtons();
+    console.log($favButtons);
+
     $allStoriesList.append($story);
   }
 
   $allStoriesList.show();
 }
+
 /** Hide stories list and display only favorite stories */
 
-function putFavStoriesOnPage(){
+function putFavStoriesOnPage() {
   $favStoriesList.empty();
   for (let story of currentUser.favorites) {
     const $story = generateStoryMarkup(story);
-    const $favButtons = generateFavoriteButtons();
+    const isFav = isFav(story);
+    const $favButtons = generateFavoriteButtons(isFav);
     $story.append($favButtons);
     $favStoriesList.append($story);
   }
@@ -91,10 +116,13 @@ function putFavStoriesOnPage(){
 $storiesContainer.on("click", ".favorite-button", handleFavoriteClick);
 
 /** Makes a call to add favorite and update currentUser from API response*/
-async function handleFavoriteClick(evt){
+async function handleFavoriteClick(evt) {
   //TODO: want to add hide button when appropriate
   const $clickTarget = $(evt.target);
   const $storyTarget = $clickTarget.closest("li");
+
+  toggleButtons($storyTarget);
+
   const storyId = $storyTarget.attr("id");
   const story = await Story.getStoryFromId(storyId);
   const userJson = await currentUser.addFavorite(story);
@@ -104,14 +132,37 @@ async function handleFavoriteClick(evt){
 $storiesContainer.on("click", ".remove-favorite-button", handleRemoveFavoriteClick);
 
 /** Makes a call to add favorite */
-async function handleRemoveFavoriteClick(evt){
+async function handleRemoveFavoriteClick(evt) {
   //TODO: want to add hide button when appropriate
   const $clickTarget = $(evt.target);
   const $storyTarget = $clickTarget.closest("li");
+
+  toggleButtons($storyTarget);
+
   const storyId = $storyTarget.attr("id");
   const story = await Story.getStoryFromId(storyId);
   const userJson = await currentUser.removeFavorite(story);
   refreshUser(userJson);
+}
+
+/** takes a jQuery object favButtons and toggles */
+function toggleButtons($li) {
+
+  const $buttons = $li.find('button');
+  console.log('toggleButtons $buttons', $buttons);
+
+  const ourButtons = [$buttons.eq(0),$buttons.eq(1)]
+
+  for (let $button of ourButtons) {
+
+    if ($button.hasClass('hidden')) {
+      $button.removeClass('hidden');
+    }
+    else {
+      $button.addClass('hidden');
+    }
+  }
+
 }
 
 $storySubmitForm.on('submit', handleStorySubmit);
